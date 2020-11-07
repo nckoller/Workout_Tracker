cbb80d4e124e11feaae83b9ca37fba6c59318155
Nina Koller <nckoller@gmail.com>
2020-11-06 20:00:48 -0600

    initial server connection running

---
+++ models/workout.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+
+const workoutSchema = new Schema({
+
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: String,
+            trim: true,
+            required: "Enter an exercise name"
+        },
+    ]
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+module.exports = Workout;
---
+++ server.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const morgan = require('morgan');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+require('./routes/htmlRoutes')(app);
+require('./routes/apiRoutes')(app);
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
