599a96a45a99b57367312ac8fdb06ff2ec690503
Nina Koller <nckoller@gmail.com>
2020-11-07 17:39:05 -0600

    adding database name for heroku deploy

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static('public'));
 require('./routes/htmlRoutes')(app);
 require('./routes/apiRoutes')(app);
 // setting up DB connection
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/ninaDB', {
   useNewUrlParser: true,
   useFindAndModify: false,
   useCreateIndex: true,
